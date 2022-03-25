b17bbf3b5757681a9eec421450e8961d8f2f3a7f
Xinyu-Li <lixinyu6688558@gmail.com>
2022-03-25 20:19:57 +1100

    remove private info from position project

--- analytics/analytics-position/src/main/java/com/monash/analytics/position/constant/ConstantValues.java
+++ analytics/analytics-position/src/main/java/com/monash/analytics/position/constant/ConstantValues.java
@@ -12,9 +12,9 @@ import java.util.Map;
 public class ConstantValues {
     public static final String POSITION_TOPIC_NAME = "position";
 
-    public static final String KAFKA_API_KEY = "J5ILXEGOXBXGLQCL";
-    public static final String KAFKA_API_SECRET = "epOgnwj/Pl33m47xdi1ZDxziwlEdUWH7axO64cavapKzMMdQgL17K2PgIXA0ZeLY";
-    public static final String KAFKA_SERVER = "pkc-ldvj1.ap-southeast-2.aws.confluent.cloud:9092";
+    public static final String KAFKA_API_KEY = "";
+    public static final String KAFKA_API_SECRET = "";
+    public static final String KAFKA_SERVER = "";
     public static final String FILE_SAVE_PATH = "C:\\develop\\saved_data\\";
     public static final String LOCAL_SERVER_NAME = "localhost"; //49.127.21.202
     public static final Map<String, String> kafkaNameMap = ImmutableMap.of(
