c1a0c7fa58bd181df58f4e3089eacdeb482dd656
Xinyu-Li <lixinyu6688558@gmail.com>
2022-03-25 20:17:36 +1100

    remove private info

--- analytics/analytics-biodata/src/main/java/com/monash/analytics/biodata/constant/ConstantValues.java
+++ analytics/analytics-biodata/src/main/java/com/monash/analytics/biodata/constant/ConstantValues.java
@@ -8,9 +8,9 @@ package com.monash.analytics.biodata.constant;
 public class ConstantValues {
 //    public static final String TOPIC_NAME = "empatica_data";
     public static final String EMPATICA_TOPIC_NAME = "empatica";
-    public static final String KAFKA_API_KEY = "J5ILXEGOXBXGLQCL";
-    public static final String KAFKA_API_SECRET = "epOgnwj/Pl33m47xdi1ZDxziwlEdUWH7axO64cavapKzMMdQgL17K2PgIXA0ZeLY";
-    public static final String KAFKA_SERVER = "pkc-ldvj1.ap-southeast-2.aws.confluent.cloud:9092";
+    public static final String KAFKA_API_KEY = "";
+    public static final String KAFKA_API_SECRET = "";
+    public static final String KAFKA_SERVER = "";
     public static final String FILE_SAVE_PATH = "C:\\develop\\saved_data\\";  // local file saving path, can be changed if need
     public static final String LOCAL_SERVER_NAME = "127.0.0.1";//""49.127.21.202";
 }
